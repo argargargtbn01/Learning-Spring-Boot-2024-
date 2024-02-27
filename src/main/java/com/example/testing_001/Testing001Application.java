7509b5afdd23ae8e2da73ba5a241e9f4ce91848e
quangnm-sonat <quangnm@sonat.vn>
2024-02-27 15:41:34 +0700

    feat: done mvc

--- src/main/java/com/example/testing_001/Testing001Application.java
+++ src/main/java/com/example/testing_001/Testing001Application.java
@@ -1,12 +1,11 @@
-package payroll;
+package com.example.testing_001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PayrollApplication {
-
+public class Testing001Application {
     public static void main(String... args) {
-        SpringApplication.run(PayrollApplication.class, args);
+        SpringApplication.run(Testing001Application.class, args);
     }
 }
